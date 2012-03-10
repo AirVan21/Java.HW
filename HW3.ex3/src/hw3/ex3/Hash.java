96f199f18142f6d05990a1ae5b4637246e74c5b9
Abramov Ivan <airvan21@gmail.com>
2012-03-10 14:23:38 +0400

    Hash class change

--- HW3.ex3/src/hw3/ex3/Hash.java
+++ HW3.ex3/src/hw3/ex3/Hash.java
@@ -2,15 +2,11 @@ package hw3.ex3;
 
 public class Hash {
     
+     public Hash() {
+        hashTable = new List[length];
+    }
+    
     private final int length = 37 ;
     
     public List hashTable[];
-    
-    public Hash() {
-        for (int i = 1; i <= length; i++) {
-            hashTable[i] = new List();
-        }
-
-    }
-    
 }
