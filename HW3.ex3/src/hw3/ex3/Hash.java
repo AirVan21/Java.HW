b742cdc47fe3c22780cafdb0b18b1c4ecf8dd4a2
Abramov Ivan <airvan21@gmail.com>
2012-03-10 17:31:27 +0400

    hash methods

--- HW3.ex3/src/hw3/ex3/Hash.java
+++ HW3.ex3/src/hw3/ex3/Hash.java
@@ -1,12 +1,49 @@
 package hw3.ex3;
 
+import javax.swing.JOptionPane;
+
 public class Hash {
-    
-     public Hash() {
+
+    public Hash() {
         hashTable = new List[length];
+        for (int i = 0; i < length; i++) {
+            hashTable[i] = new List();
+        }
     }
     
-    private final int length = 37 ;
-    
     public List hashTable[];
+
+    public void addElement(String value) {
+        int codeValue = Math.abs(value.hashCode() % length);
+        hashTable[codeValue].addToEnd(value);
+    }
+
+    public void deleteElement(String value) {
+        int codeValue = Math.abs(value.hashCode() % length);
+        if (hashTable[codeValue].listEntry(value)) {
+            hashTable[codeValue].deleteElement(value);
+        } else {
+            JOptionPane.showMessageDialog(null, "This element wasn't found !");
+        }
+    }
+
+    public void hashEntry(String value) {
+        int codeValue = Math.abs(value.hashCode() % length);
+        if (hashTable[codeValue].listEntry(value)) {
+            JOptionPane.showMessageDialog(null, "Yes, it's here!");
+        } else {
+            JOptionPane.showMessageDialog(null, "This element wasn't found !");
+        }
+
+    }
+
+    public void hashOutput() {
+        for (int i = 0; i < length; i++) {
+            if (!hashTable[i].isEmpty()) {
+                hashTable[i].output();
+            }
+        }
+         JOptionPane.showMessageDialog(null, "Take a look at the console!");
+    }
+    private final int length = 31;
 }
